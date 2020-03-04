6bc14203eead50cf3aa2e0147b3d2c7dff3e2219
kato <itizisiyouitasimasu@hotmail.co.jp>
2020-03-04 13:33:06 +0000

    first commit

---
+++ app/Http/Controllers/DogorcatController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DogorcatController extends Controller
+{
+    //
+    public function index()
+    {
+      return view('index');
+    }
+}
